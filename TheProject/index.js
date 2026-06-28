1630d1d1c7ae31ba848f7d6cb0a285ecfce776e2
Eetu Jukka <eetujukka@icloud.com>
2026-06-28 09:55:47 +0000

    1.5 done

--- TheProject/index.js
+++ TheProject/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("*", (_req, res) => {
-  res.send("Todo app");
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
