65db2a455b36c32036a869fdc8c940a586d327bb
Eetu Jukka <eetujukka@icloud.com>
2026-07-12 15:47:57 +0000

    2.5

--- Logger/LogOutput/server/index.js
+++ Logger/LogOutput/server/index.js
@@ -1,22 +1,29 @@
+import crypto from 'crypto';
 import axios from 'axios';
 import express from 'express';
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const directory = path.join('/', 'app', 'config');
+const filePath = path.join(directory, 'information');
+
 const string = crypto.randomUUID();
 
 app.get('/', async (_req, res) => {
+    const firstLine = `file content: ${readFileSync(filePath, 'utf8').trim()}`;
+    const secondLine = `env variable: MESSAGE=${process.env.MESSAGE}`;
+
     const timestamp = new Date().toISOString();
-    const firstLine = `${timestamp} ${string}\n`;
+    const thirdLine = `${timestamp} ${string}`;
 
-    const result = await axios.get("http://pingpong-svc:6789")
+    const result = await axios.get('http://pingpong-svc:6789');
 
-    const secondLine = `Ping / Pongs: ${result.data}`
+    const fourthLine = `Ping / Pongs: ${result.data}`;
 
-    res.type('text/plain').send(`${firstLine}\n${secondLine}`);
+    res.type('text/plain').send(`${firstLine}\n${secondLine}\n${thirdLine}\n${fourthLine}`);
 });
 
 
